87b134335534fadb88789e2c315ce92051ace18c
Chintan Shah <chintanhappiness@gmail.com>
2014-12-23 00:28:49 +0530

    Hauth implemented

--- dell.sql
+++ dell.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Dec 22, 2014 at 07:38 PM
--- Server version: 5.5.32
--- PHP Version: 5.4.19
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `dell`
---
-CREATE DATABASE IF NOT EXISTS `dell` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `dell`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `accesslevel`
---
 
 CREATE TABLE IF NOT EXISTS `accesslevel` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
