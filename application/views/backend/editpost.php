046b41537dbc2b3e22fd7606fef29776f5e75847
Chintan Shah <chintanhappiness@gmail.com>
2015-01-07 23:02:03 +0530

    post create edit

--- application/views/backend/editpost.php
+++ application/views/backend/editpost.php
@@ -9,10 +9,17 @@
 				<div class="form-group">
 				  <label class="col-sm-2 control-label" for="normal-field">Text</label>
 				  <div class="col-sm-4">
-					<input type="text" id="normal-field" class="form-control" name="text" value="<?php echo set_value('text',$before->text);?>">
+					
+				  <textarea name="text"  class="form-control contenttext"><?php echo set_value('text',$before->text);?></textarea>
+				  <div class="onlytwitter">
+					    <p>
+					        Characters Remaining: <span class="textcharacters">140</span>
+					    </p>
+					</div>
+<!--					<input type="text" id="normal-field" class="form-control" name="text" value="">-->
 				  </div>
 				</div>
-				<div class=" form-group">
+				<div class=" form-group onlyfb">
 				  <label class="col-sm-2 control-label" for="normal-field">Image</label>
 				  <div class="col-sm-4">
 					<input type="file" id="normal-field" class="form-control" name="image" value="<?php echo set_value('image',$before->image);?>">
@@ -31,7 +38,7 @@
 				  <div class="col-sm-4">
 					<?php
 						
-						echo form_dropdown('posttype',$posttype,set_value('posttype',$before->posttype),'class="chzn-select form-control" 	data-placeholder="Choose a Accesslevel..."');
+						echo form_dropdown('posttype',$posttype,set_value('posttype',$before->posttype),'class="chzn-select formplatform form-control" 	data-placeholder="Choose a Accesslevel..."');
 					?>
 				  </div>
 				</div>
@@ -46,3 +53,46 @@
 			  </form>
 			</div>
 		</section>
+		<script>
+    var maxchars=140;
+    function cutstring() {
+        var textlength=$(".contenttext").val().length;
+        var platform=$(".formplatform").val();
+        if(textlength>140 && platform==2)
+        {
+            var newstring=$(".contenttext").val();
+            newstring=newstring.substring(0,140);
+            $(".contenttext").val(newstring);
+            $(".textcharacters").text(0);
+        }
+        
+    };
+    $(document).ready(function() {
+        $(".formplatform").change(function() {
+            var platform=$(this).val();
+            if(platform==1)
+            {
+                $(".onlyfb").show(100);
+                $(".onlytwitter").hide(100);
+            }
+            else if(platform==2)
+            {
+                $(".onlyfb").hide(100);
+                $(".onlytwitter").show(100);
+                cutstring();
+            }
+        });
+        $(".contenttext").keyup(function() {
+            var textlength=$(this).val().length;
+            
+            $(".textcharacters").text(140-textlength);
+            var platform=$(".formplatform").val();
+            cutstring();
+            
+        });
+        
+        $(".formplatform").trigger("change");
+        $(".contenttext").trigger("keyup");
+    });
+   
+</script>
