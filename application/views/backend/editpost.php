ab3d649bc6c363da8da0960951dc318aa195d894
Chintan Shah <chintanhappiness@gmail.com>
2015-01-07 23:04:25 +0530

    platform

--- application/views/backend/editpost.php
+++ application/views/backend/editpost.php
@@ -34,7 +34,7 @@
 				</div>
 				
 				<div class=" form-group">
-				  <label class="col-sm-2 control-label">posttype</label>
+				  <label class="col-sm-2 control-label">Platform</label>
 				  <div class="col-sm-4">
 					<?php
 						
